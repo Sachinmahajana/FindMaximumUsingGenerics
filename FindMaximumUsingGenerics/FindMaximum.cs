cea5901262068bf363e6d7b9857573aee4f7830c
Sachinmahajana <sachinmahajan782@gmail.com>
2023-02-07 20:56:04 +0530

    [Sachin] Adding UC1-FindMaxInt

---
+++ FindMaximumUsingGenerics/FindMaximum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumUsingGenerics
+{
+    internal class FindMaximum
+    {
+        public int first, second, third;
+      public static void FindMaxValue(int first,int second,int third)
+      {
+            if(first.CompareTo(second) >=0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is greater", first);
+            }
+            else if(second.CompareTo(first) >=0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is greater", second);
+            }
+            else
+            {
+                Console.WriteLine("{0} is greater", third);
+            }
+       }
+
+    }
+
+}
+            
--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -12,6 +12,15 @@ namespace FindMaximumUsingGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to FindMaximumGenerics Program");
+            Console.WriteLine("Enter the first Number: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second Number: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third Number: ");
+            int third = Convert.ToInt32(Console.ReadLine());
+
+            FindMaximum.FindMaxValue(first, second, third);
+           
             Console.ReadLine();
         }
        
