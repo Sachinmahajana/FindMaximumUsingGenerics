d14145b800b447a8f08eb20e70c624644d164ec7
Sachinmahajana <sachinmahajan782@gmail.com>
2023-02-07 18:07:04 +0530

    [Sachin] Adding the Welcome message

---
+++ FindMaximumUsingGenerics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to FindMaximumGenerics Program");
+            Console.ReadLine();
+        }
+       
+    }
+}
